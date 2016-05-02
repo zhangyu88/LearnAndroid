4fce78fd20a9211411d99fa911eaab3e505bdb3f
zhangyu88 <cn_zhangyu@hotmail.com>
2016-05-03 00:43:30 +0800

    This is my end file

--- app/src/main/java/learnandroid/example/com/learnandroid/MainActivity.java
+++ app/src/main/java/learnandroid/example/com/learnandroid/MainActivity.java
@@ -2,6 +2,10 @@ package learnandroid.example.com.learnandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import learnandroid.example.com.learnandroid.utilities.MyDemo1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getMyInfo();
+
+        for (int counter = 0; counter < 10; counter++) {
+
+            Log.i("MainActivity", "value: " + counter);
+        }
+
+
+
+    }
+
+    private void getMyInfo() {
+        // show my define string
+        MyDemo1 mystr1 = new MyDemo1();
+
+        TextView tv = (TextView) findViewById(R.id.myTextView);
+
+        tv.setText(mystr1.getValue());
     }
 }
---
+++ app/src/main/java/learnandroid/example/com/learnandroid/utilities/MyDemo1.java
@@ -0,0 +1,18 @@
+package learnandroid.example.com.learnandroid.utilities;
+
+/**
+ * Created by ZhangYu on 2016/5/2.
+ * This class use demo java codeing
+ */
+public class MyDemo1 {
+
+    public String getValue() {
+
+
+        return "Hello from the MyDemo1";
+
+    }
+
+
+
+}
